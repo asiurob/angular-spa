9b8f4fc6ca1307d9ef83a4efb04a58297c9c7709
Siurob <ing.asiurob@gmail.com>
2018-11-27 17:30:29 -0600

    Nuevo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavarComponent } from './components/navar/navar.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavarComponent,
+    HomeComponent,
+    AboutComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
